e1dca3d556efcbba0f9dd5811410e906fbae8e52
Sylar An <sylar.an@china.local>
2024-07-15 18:03:27 +0800

    Site updated: 2024-07-15 18:03:25

---
+++ sitemap_baidu.js
@@ -0,0 +1,6 @@
+//<meta name="baidu-site-verification" content="codeva-3QNQJ1nG76" />
+// 插入 meta 标签
+var oMeta = document.createElement('meta');
+oMeta.content = 'codeva-3QNQJ1nG76';
+oMeta.name = 'baidu-site-verification';
+document.getElementsByTagName('head')[0].appendChild(oMeta);
